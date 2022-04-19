08740ce9684c4b94c9c9b363afe0edf2d01df0d4
Constantin <grishanoff.konstantin@yandex.ru>
2022-04-19 15:46:55 +0500

    dop1 compl

---
+++ Dop1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace ConsoleApp
+{
+    class Programm
+    {
+        static void Main(string[] args)
+
+        { // Даны два числа a, b. Сложите их, используя только операции инкремента и декремента.
+
+
+            Console.WriteLine("Введите первое число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Recursion();
+          
+
+
+            void Recursion()
+            {
+                if (a>0)
+                {
+                    a--;
+                    b++;
+                    Recursion();
+                }
+                else
+                {
+                    Console.WriteLine($"Сумма чисел равна: {b} ");
+                    return;
+                } 
+                
+            }
+
+        }
+
+    }
+}
