494da042df45ce607036fa446411d8767f2d2ec2
Constantin <grishanoff.konstantin@yandex.ru>
2022-04-21 08:50:59 +0500

    Ex1 compl

---
+++ sem11/Ex1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace ConsoleApp
+{
+    class Programm
+    {
+        static void Main(string[] args)
+
+        { 
+            // Задача 1. На вход подуются два числа n и m, 
+            // такие, что n<m. Заполните массив случайными числами из промежутка [n, m].
+
+
+            int[] array = new int[10];
+            Console.WriteLine("Введите числа");
+            Recursion();
+
+
+            void Recursion()
+            {
+
+                int n = Convert.ToInt32(Console.ReadLine());
+                int m = Convert.ToInt32(Console.ReadLine());
+
+
+                if (n>=m)
+                {
+                    Console.WriteLine("Числа введены неверно. Введите заново.");
+                    Recursion();
+                }
+
+                else 
+                {
+                    var rand = new Random();
+                    int count = 0;
+                    void Recursion_2()
+                    {
+                        if (count<10)
+                        {
+                            array[count] = rand.Next(n,m);
+                            Console.Write($"  {array[count]}  ");
+                            count++;
+                            Recursion_2();
+                        }
+                        else return;
+                    }
+                    Recursion_2();
+                    return;
+
+                }
+            }
+
+
+
+            
+        }
+
+    }
+}
